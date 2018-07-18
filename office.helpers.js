826faf50957c8a1a8189e00171b877e1a7d0033d
pgoumas <pgoumas@hotmail.com>
2018-07-18 15:31:21 +0200

    tried to fix Promise line 2909

--- office.helpers.js
+++ office.helpers.js
@@ -2906,7 +2906,10 @@ var __extends = this && this.__extends || function () {
     };
 }();
 var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
-    return new (P))(function (resolve, reject) {
+    if (Promise == null) {
+	    var Promise ='';
+    }
+	return new (P || (P = Promise))(function (resolve, reject) {
         function fulfilled(value) {
             try {
                 step(generator.next(value));
